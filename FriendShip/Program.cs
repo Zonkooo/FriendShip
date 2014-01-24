274f1f4e70bb8af4cfc07e6c585fada5c6f6a87d
kameron <kameron.argis@gmail.com>
2014-01-25 00:23:50 +0100

    removed useless comment

--- FriendShip/Program.cs
+++ FriendShip/Program.cs
@@ -2,9 +2,6 @@ namespace FriendShip
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		static void Main(string[] args)
 		{
 			using (GameCore game = new GameCore())
