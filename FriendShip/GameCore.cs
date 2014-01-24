e47daaa1268301c3eb3b7986ee8592c4674c54ed
Raphael Vandon <r.vandon@criteo.com>
2014-01-24 21:37:30 +0100

    initial commit

---
+++ FriendShip/GameCore.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FriendShip
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class GameCore : Game
+	{
+		public GraphicsDeviceManager graphics;
+		public SpriteBatch spriteBatch;
+		public Texture2D OneWhitePixel;
+
+		public GameCore()
+		{
+			graphics = new GraphicsDeviceManager(this)
+			{
+				PreferredBackBufferHeight = 720,
+				PreferredBackBufferWidth = 1280,
+			};
+			//IsMouseVisible = true;
+
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			OneWhitePixel = Content.Load<Texture2D>("onewhitepixel");
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape) || GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+			{
+				this.Exit();
+			}
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Gold);
+
+			base.Draw(gameTime);
+
+			if(spriteBatch != null)
+			{
+				spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+				//spriteBatch.Draw(levelMgr.SpecialDisplay, new Vector2(-(float)Math.Cos(-GameCore.Angle), -(float)Math.Sin(-GameCore.Angle)) * 18, Color.White);
+				spriteBatch.End();
+			}
+		}
+
+		/// <summary>
+		/// Spritebatch must be initialized (begin) before
+		/// </summary>
+		public void DrawHitBox(Rectangle r)
+		{
+			//spriteBatch.Draw(OneWhitePixel, this.ToDrawPosition(new Vector2(r.X, r.Y), Ground.Position), null, new Color(255, 0, 0, 8), (float) Angle, new Vector2(), new Vector2(r.Width, r.Height), SpriteEffects.None, 0);
+		}
+	}
+}
