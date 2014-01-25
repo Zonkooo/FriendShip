28ec3855fcce0d45958edfb42fed7bcd7d42fc35
kameron <kameron.argis@gmail.com>
2014-01-25 01:22:28 +0100

    add Players Moves

--- FriendShip/Player.cs
+++ FriendShip/Player.cs
@@ -14,6 +14,7 @@ namespace FriendShip
 		public Vector2 Position;
 		private Texture2D _texture;
 		private GameCore _game;
+        public const int moveSpeed = 5;
 
 		public Player (GameCore game, Texture2D texture)
 			: base(game)
@@ -26,6 +27,25 @@ namespace FriendShip
 			game.Components.Add (this);
 		}
 
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState currentKeyState = Keyboard.GetState();
+            var Delta = new Vector2();
+            if (currentKeyState.IsKeyDown(Keys.Left))
+                Delta.X = -moveSpeed;
+
+            if (currentKeyState.IsKeyDown(Keys.Right))
+                Delta.X = moveSpeed;
+
+            if (currentKeyState.IsKeyDown(Keys.Up))
+                Delta.Y = -moveSpeed;
+
+            if (currentKeyState.IsKeyDown(Keys.Down))
+                Delta.Y = moveSpeed;
+            Position = Position + Delta;
+
+            //base.Update(gameTime);
+        }
 		public override void Draw (GameTime gameTime)
 		{
 			if (_game.spriteBatch != null)
