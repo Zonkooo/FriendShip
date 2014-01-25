4e71425b9675fae6d6c47181801735f6d782c23d
Raphael Vandon <r.vandon@criteo.com>
2014-01-25 19:51:02 +0100

    split event class

---
+++ FriendShip/Events/AllToCale.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+
+namespace FriendShip
+{
+
+	public class AllToCale : EventBase
+	{
+		private readonly Room _target;
+
+		public AllToCale (GameCore game)
+			:base(game, "Everyone in the storage room !")
+		{
+			_target = _game._rooms [RoomType.MACHINE];
+		}
+
+		public override void Update (GameTime gameTime)
+		{
+			bool missingPlayer = false;
+			foreach(var player in _game.Players)
+			{
+				if (player.Enabled && player.currentRoom != _target)
+				{
+					missingPlayer = true;
+					break;
+				}
+			}
+
+			if (missingPlayer)
+				_game.health -= 0.0001f;
+			else
+				this.Enabled = false;
+		}
+
+		public override void DrawText (SpriteBatch sb)
+		{
+			sb.DrawString (_game.font, _text, new Vector2 (410, 50), Color.White);
+		}
+	}
+
+}
---
+++ FriendShip/Events/MustDriveShip.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+
+namespace FriendShip
+{
+
+	public class MustDriveShip : EventBase
+	{
+		private readonly Room _target;
+		bool missingPlayer;
+
+		public MustDriveShip (GameCore game, string text)
+			:base(game, text)
+		{
+			_target = _game._rooms [RoomType.COMMANDS];
+			Enabled = true; //always enabled
+		}
+
+		public override void Update (GameTime gameTime)
+		{
+			missingPlayer = true;
+			foreach(var player in _game.Players)
+			{
+				if (player.Enabled && player.currentRoom == _target)
+				{
+					missingPlayer = false;
+					break;
+				}
+			}
+
+			if (missingPlayer)
+				_game.derive -= 0.001f;
+			else
+				_game.derive = Math.Min(_game.derive + 0.0005f, 1f);
+		}
+
+		public override void DrawText (SpriteBatch sb)
+		{
+			if(missingPlayer)
+				sb.DrawString (_game.font, _text, new Vector2 (410, 100), Color.Crimson);
+		}
+	}
+}
